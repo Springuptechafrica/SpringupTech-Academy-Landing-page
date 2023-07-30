b495421b05e4dcca89e839dd384d0fb673f56efb
Josiah Obaje <Josiah.Obaje@wemabank.com>
2023-07-30 01:55:43 +0100

    Add dynamic side bar to the dashboard

--- src/registrationform.js
+++ src/registrationform.js
@@ -1,29 +1,33 @@
-import { Box, Container, FormLabel, Input, Text } from '@chakra-ui/react'
+import { Box, Container, Flex, FormLabel, Input, Text } from '@chakra-ui/react'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function Registrationform() {
   return (
-    <Container maxW="container.lg" mt={16} background={'teal.100'}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
-        {' '}
-        <Box maxW="lg" borderWidth="5px" alignItems="center">
-          {' '}
-          <div>
-            {' '}
-            <Text fontSize="4xl" as="kbd" align={'center'} mb={8}>
-              {' '}
-              <h1>Registration Form</h1>{' '}
-            </Text>{' '}
-          </div>{' '}
-        </Box>{' '}
-      </div>
-    </Container>
+    <Flex>
+        <Sidebar />
+    </Flex>
+    // <Container maxW="container.lg" mt={16} background={'teal.100'}>
+    //   <div
+    //     style={{
+    //       display: 'flex',
+    //       justifyContent: 'center',
+    //       alignItems: 'center',
+    //       height: '100vh',
+    //     }}
+    //   >
+    //     {' '}
+    //     <Box maxW="lg" borderWidth="5px" alignItems="center">
+    //       {' '}
+    //       <div>
+    //         {' '}
+    //         <Text fontSize="4xl" as="kbd" align={'center'} mb={8}>
+    //           {' '}
+    //           <h1>Registration Form</h1>{' '}
+    //         </Text>{' '}
+    //       </div>{' '}
+    //     </Box>{' '}
+    //   </div>
+    // </Container>
   )
 }
 
